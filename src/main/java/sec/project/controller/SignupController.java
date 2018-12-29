fac058d7a8cbb020395208b065ebb6ea2c946eed
magamala <46202527+magamala@users.noreply.github.com>
2018-12-29 09:42:37 +0200

    Update SignupController.java

--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -1,10 +1,12 @@
 package sec.project.controller;
 
+import java.util.Objects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import sec.project.domain.Signup;
 import sec.project.repository.SignupRepository;
 
@@ -26,8 +28,38 @@ public class SignupController {
 
     @RequestMapping(value = "/form", method = RequestMethod.POST)
     public String submitForm(@RequestParam String name, @RequestParam String address) {
-        signupRepository.save(new Signup(name, address));
-        return "done";
+        if(!signupRepository.findByName(name).isEmpty()){
+            return "redirect:/duplicate?name=" + name;
+        } else {
+            signupRepository.save(new Signup(name, address));
+            return "done";
+        }
+    }
+    
+    @RequestMapping(value = "/duplicate", method = RequestMethod.GET)
+    @ResponseBody
+    public String duplicate(@RequestParam String name){
+        if (signupRepository.findByName(name).isEmpty()){
+            return name + " has not been registered.";
+        } else {
+            return name + " has already been registered with address " +
+                signupRepository.findByName(name).get(0).getAddress() + ".";
+        }
+    }
+    @RequestMapping(value = "/reset", method = RequestMethod.GET)
+    public String loadPasswordForm() {
+        return "reset";
+        
+    }
+    @RequestMapping(value = "/reset", method = RequestMethod.POST)
+    @ResponseBody
+    public String submitPasswordForm(@RequestParam String password, @RequestParam String confirm) {
+        if(Objects.equals(password, confirm){
+            return "Password was resetted;)";
+        } else {
+            return "Password not confirmed";
+        }
+           
     }
 
 }
